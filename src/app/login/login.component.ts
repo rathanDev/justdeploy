a4731a77a8fee7b32902802360ddf8872143b4d4
Jana <rathan.kparam@gmail.com>
2024-12-08 20:45:13 +0800

    leverage observable

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,20 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Subscription } from 'rxjs';
 import { LoginService } from '../service/login.service';
+import { SharedService } from '../service/shared.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit, OnDestroy {
   loginForm: FormGroup;
+  loggedIn: boolean = false;
+  loginSubscription: Subscription | undefined;
 
-  constructor(private fb: FormBuilder, private loginService: LoginService) {
+  constructor(
+    private fb: FormBuilder,
+    private loginService: LoginService,
+    private sharedService: SharedService
+  ) {
     this.loginForm = this.fb.group({
       email: ['', [Validators.required]],
       password: ['', [Validators.required]],
     });
+  }
+
+  ngOnInit() {
+    this.loginSubscription = this.sharedService.userLoggedIn$.subscribe(
+      (loggedIn) => (this.loggedIn = loggedIn)
+    );
+  }
+
+  ngOnDestroy() {
+    if (this.loginSubscription) {
+      this.loginSubscription.unsubscribe();
+    }
   }
 
   onSubmit() {
