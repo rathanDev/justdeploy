ce84e14ef98666c5ee538b2e09dcc5ff3cdd9a9a
Jana <rathan.kparam@gmail.com>
2024-12-09 00:01:13 +0800

    Save availability

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { Subscription } from 'rxjs';
+import { RegistrationInfo } from '../model/registrationInfo.model';
 import { DataService } from '../service/data.service';
 import { SharedService } from '../service/shared.service';
-import { RegistrationInfo } from '../model/registrationInfo.model';
 
 @Component({
   selector: 'app-profile',
@@ -13,10 +14,17 @@ export class ProfileComponent implements OnInit, OnDestroy {
   loggedInUser: RegistrationInfo | undefined;
   sub: Subscription | undefined;
 
+  availabilityForm: FormGroup;
+
   constructor(
+    private fb: FormBuilder,
     private dataService: DataService,
     private sharedService: SharedService
-  ) {}
+  ) {
+    this.availabilityForm = this.fb.group({
+      av: ['', []],
+    });
+  }
 
   ngOnInit() {
     this.sub = this.sharedService.loggedInUser$.subscribe(
@@ -31,4 +39,36 @@ export class ProfileComponent implements OnInit, OnDestroy {
       this.sub.unsubscribe();
     }
   }
+
+  saveAvailability() {
+    const text = this.availabilityForm.value.av;
+    const lines = text.split("\n");
+
+    for(const line of lines) {
+      const dateRange = this.parseLine(line);
+      console.log(dateRange);
+    }
+
+  }
+
+  parseLine(line: string): { from: Date; to: Date } | null {
+    const regex = /^(\d{4}-\d{2}-\d{2})from(\d{2}:\d{2}:\d{2})to(\d{2}:\d{2}:\d{2})$/;
+    const match = line.match(regex);
+
+    if (match) {
+      const datePart = match[1];
+      const fromTime = match[2];
+      const toTime = match[3];
+
+      const from = new Date(`${datePart}T${fromTime}`);
+      const to = new Date(`${datePart}T${toTime}`);
+
+      if (!isNaN(from.getTime()) && !isNaN(to.getTime())) {
+        return { from, to };
+      }
+    }
+
+    return null; // Return null if the line doesn't match or is invalid
+  }
+
 }
--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -1,17 +1,12 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
+import { ReactiveFormsModule } from '@angular/forms';
 import { ProfileRoutingModule } from './profile-routing.module';
 import { ProfileComponent } from './profile.component';
 
-
 @NgModule({
-  declarations: [
-    ProfileComponent
-  ],
-  imports: [
-    CommonModule,
-    ProfileRoutingModule
-  ]
+  declarations: [ProfileComponent],
+  imports: [CommonModule, ProfileRoutingModule, ReactiveFormsModule],
 })
-export class ProfileModule { }
+export class ProfileModule {}
