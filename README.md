ff022c835d7944a49752fbfe1502eef46e1fb60d
Jana <rathan.kparam@gmail.com>
2024-12-08 17:13:22 +0800

    Init modules

--- README.md
+++ README.md
@@ -5,3 +5,17 @@ ng b --output-path docs --base-href /justdeploy/
 rathandev.github.io/justdeploy
 
 ng new justdeploy --directory . --no-standalone --routing 
+
+ng g m login --route login --module app.module
+ng g m home --route home --module app.module
+ng g m profile --route profile --module app.module
+
+
+
+
+
+
+
+
+
+
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
+  },
+  {
+    path: 'login',
+    loadChildren: () =>
+      import('./login/login.module').then((m) => m.LoginModule),
+  },
+  {
+    path: 'profile',
+    loadChildren: () =>
+      import('./profile/profile.module').then((m) => m.ProfileModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
